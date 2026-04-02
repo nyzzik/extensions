624690adfc34c86ef4d03a10182227d1bad593e1
Josh Biedrzycki <nyzzik@gmail.com>
2026-04-02 06:16:02 -0400

    Bump version

--- src/AsuraScans/pbconfig.ts
+++ src/AsuraScans/pbconfig.ts
@@ -3,7 +3,7 @@ import { ContentRating, SourceIntents, type ExtensionInfo } from "@paperback/typ
 export default {
     name: "Asura Scans",
     description: "Extension that pulls content from asurascans.com.",
-    version: "1.0.0-alpha.23",
+    version: "1.0.0-alpha.24",
     icon: "icon.png",
     language: "en",
     contentRating: ContentRating.EVERYONE,
